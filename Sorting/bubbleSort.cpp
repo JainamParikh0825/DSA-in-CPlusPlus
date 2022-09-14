1e249dd212f70820e45974978ca0dafd88c7a06a
Jainam Parikh <jainamparikh0825@gmail.com>
2022-09-14 16:20:20 +0530

    update: bubble sort

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+    for (int i = 0; i < n; ++i){
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+
+    for (int i = 0; i < n - 1; ++i){
+        for (int j = 0; j < n - 1 - i; ++j){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+
+    for(int value : arr){
+        cout << value << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
