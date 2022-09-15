a9cfc94606edad5bc5d5b1a1133b36531251fbdc
Jainam Parikh <jainamparikh0825@gmail.com>
2022-09-15 22:51:19 +0530

    update

---
+++ Linked List/basic.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+/* Singly Linked List */
+class SNode{
+    int data;
+    SNode *next;
+};
+
+/* Doubly Linked List */
+class DNode{
+    int data;
+    DNode *prev;
+    DNode *next;
+};
+
+int main(){
+    return 0;
+}
